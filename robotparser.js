f9ec3c372d335363c5ff0773487de1dc5a4aa9b7
trovati <watsontrovati@gmail.com>
2020-07-17 23:47:22 -0300

    robotsParser

--- robotparser.js
+++ robotparser.js
@@ -1,21 +1,19 @@
 //https://github.com/samclarke/robots-parser retirado desse github
-var robotsParser = require('robots-parser');
+const robotParser = require('robots-parser');
+const request = require("request-promise");
 
-var robots = robotsParser(
-    'http://www.example.com/robots.txt', 
-    [
-	'User-agent: *',
-	'Disallow: /dir/',
-	'Disallow: /test.html',
-	'Allow: /dir/test.html',
-	'Allow: /test.html',
-	'Crawl-delay: 1',
-	'Sitemap: http://example.com/sitemap.xml',
-	'Host: example.com'
-].join('\n'));
+const robotsUrl = "https://textfiles.meulie.net/robots.txt";
 
-robots.isAllowed('http://www.example.com/test.html', 'Sams-Bot/1.0'); // false
-robots.isAllowed('http://www.example.com/dir/test.html', 'Sams-Bot/1.0'); // true
-robots.isDisallowed('http://www.example.com/dir/test2.html', 'Sams-Bot/1.0'); // true
-robots.getCrawlDelay('Sams-Bot/1.0'); // 1
-robots.getSitemaps(); // ['http://example.com/sitemap.xml']
+async function getRobotsTxt(robotsUrl) {
+    const robotTxt = await request.get(robotsUrl);
+    const robots = robotParser(robotsUrl, robotTxt);
+    console.log(robots.isAllowed("https://textfiles.meulie.net/100/", "WatsonBot")
+    );
+
+    console.log(robots.isAllowed("https://textfiles.meulie.net/100/", "rogerbot")
+    );
+
+   
+}
+
+getRobotsTxt(robotsUrl);
